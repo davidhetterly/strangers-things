f01d2b4cd265a7a359c97ac65dda3528b338991b
David Hetterly <dave.hetterly@gmail.com>
2023-09-17 22:12:20 -0700

    Building Home and Post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,27 @@
+import { useState, useEffect } from "react";
+import { fetchPosts } from "../API";
+import Post from "./Post";
+
 export default function Posts() {
-    return <div>Posts</div>;
-  }
-  
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const data = await fetchPosts();
+      console.log(data);
+      setPosts(data.data.posts);
+    }
+    getPosts();
+  }, []);
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <div className="posts-container">
+        {posts.map((post) => {
+          return <Post key={post._id} {...post}  />;
+        })}
+      </div>
+    </>
+  );
+}
